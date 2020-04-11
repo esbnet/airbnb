f8ec8f1ddaff113c048f53d01cd62af2b457da97
Edmilson Soares <esbnet@gmail.com>
2020-04-11 19:42:57 -0300

    Autenticação JWT e API REST

--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -14,8 +14,9 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+'use strict'
+
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
---
+++ readme.md
@@ -0,0 +1,6 @@
+=AirBnB
+
+Aplicação cliente/servidor com Adonis, ReactJS e React Native
+
+Lucid ORM - abstração lógica de acesso a dados
+JWT - autenticação
